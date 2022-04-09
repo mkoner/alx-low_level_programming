4b84931bc729d2c55aad9258cb1e1ff77b5cc49b
mkoner <amzid@hotmail.fr>
2022-04-08 19:50:47 -0700

    print digit of the base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf ("%d", i);
+	printf ("\n");
+	return (0);
+}
+
