57dbd63f6e2b14aa720645ab592c0d83c11f2ac4
mkoner <amzid@hotmail.fr>
2022-04-08 20:19:32 -0700

    combination of digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(32);
+		putchar(44);
+	}
+	putchar (10);
+	return (0);
+}
