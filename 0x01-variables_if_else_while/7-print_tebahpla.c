b0b07aa755ea01a61d11d9eb2c7a10eaaef0f19f
mkoner <amzid@hotmail.fr>
2022-04-08 20:02:35 -0700

    alphabets in reversed mode

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+		putchar(i);
+	putchar (10);
+	return (0);
+}
