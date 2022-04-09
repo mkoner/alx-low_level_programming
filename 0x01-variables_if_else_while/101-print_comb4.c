6614476232ad29e2992daa93c2876fb33a42380b
mkoner <amzid@hotmail.fr>
2022-04-09 05:31:25 -0700

    printing combination of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,12 +16,12 @@ int main(void)
 
 	for (i = 48; i < 56; i++)
 		for (j = i + 1; j < 57; j++)
-			for (k = j+1; k < 58; k++)
+			for (k = j + 1; k < 58; k++)
 			{
 			putchar(i);
 			putchar(j);
 			putchar(k);
-			if (i == 55 && j == 56 && k== 57)
+			if (i == 55 && j == 56 && k == 57)
 				continue;
 			{
 			putchar(44);
