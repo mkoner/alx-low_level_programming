2b026c8a4be9d680856d315fdd8d76ae8497ea64
mkoner <amzid@hotmail.fr>
2022-04-09 05:25:54 -0700

    printing combination of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 56; i++)
+		for (j = i + 1; j < 57; j++)
+			for (k = j+1; k < 58; k++)
+			{
+			putchar(i);
+			putchar(j);
+			if (i == 56 && j == 57)
+				continue;
+			{
+			putchar(44);
+			putchar(32);
+			}
+		}
+	putchar(10);
+	return (0);
+}
