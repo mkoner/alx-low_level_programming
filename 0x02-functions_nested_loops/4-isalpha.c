b2d67f40fda3f2ae86c15c00c90942ad8c8264bc
mkoner <amzid@hotmail.fr>
2022-04-12 05:48:39 -0700

    check if cahr is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check if alphabet char.
+ * @c: entered parameter
+ *
+ * Return: 1 or 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122 && c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
