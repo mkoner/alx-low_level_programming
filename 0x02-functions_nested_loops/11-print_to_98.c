ad1990f133b7ac1d7fe456041ecdf8757396f9df
mkoner <amzid@hotmail.fr>
2022-04-12 12:36:35 -0700

    print natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print natural numbers from n to 98
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	int i,j;
+
+	for (i = n; i < 98; i++)
+	{
+		if (i == 1)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+		for (j = 2; j <= 49; j++)
+		{
+			int c=0;
+
+			if (n % j == 0)
+			{
+				c=1;
+				break;
+			}
+			if (c == 0)
+			{
+				_putchar(i);
+				_putchar(',');
+				_putchar(' ');
+			}
+}
