1abf297fa794f61dd813b70e3c38faa322ec087c
mkoner <amzid@hotmail.fr>
2022-04-12 09:48:16 -0700

    last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,10 +2,13 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- *
+ * @n: given number
  * Return: the last digit
  */
 int print_last_digit(int n)
 {
-	return (n%10);
+	if (n < 0)
+		return (-n % 10);
+	else
+		return (n % 10);
 }
