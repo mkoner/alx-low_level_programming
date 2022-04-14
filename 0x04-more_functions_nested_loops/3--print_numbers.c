d23f5d5035067166203b207f7008b428dc8b1cf8
mkoner <amzid@hotmail.fr>
2022-04-14 09:36:14 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3--print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numers from 0 to 9
+ *
+ * Return: Always numbers.
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	do {
+		_putchar(i);
+		i++
+	} while (i < 57);
+}
+
