1e5b7e65ee29fdf0ff973232fd191b2f48c6b0be
mkoner <amzid@hotmail.fr>
2022-04-14 11:01:30 -0700

    print a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,8 +7,19 @@
  */
 void print_line(int n)
 {
-	int i;
-	for (i = 0; i < n; i++)
-		_putchar(45);
-	_putchar(10);
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
 }
