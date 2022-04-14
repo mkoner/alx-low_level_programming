9461631ed6960327007e6903322b121a74dbacc0
mkoner <amzid@hotmail.fr>
2022-04-14 10:39:49 -0700

    print 10 time 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,14 @@ void more_numbers(void)
 	for (i = 0; i < 9; i++)
 	{
 		for (j = 0; j < 15; j++)
-			_putchar(j + '0');
+		{
+			if (y > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+
+			_putchar((j % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }
