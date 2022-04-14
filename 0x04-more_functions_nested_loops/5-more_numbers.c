c1385303b8fd30f14091fa82e0dc9237f3f2fbf0
mkoner <amzid@hotmail.fr>
2022-04-14 10:32:14 -0700

    print 10 time 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+i#include "main.h"
+
+/**
+ * more_numbers - prints 10 times 0-14
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i , j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j < 15; j++)
+			_putchar(i + '0');
+		_putchar('\n');
+	}
+}
