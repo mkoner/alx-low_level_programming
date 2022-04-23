efc0158868ab07879b2780829673db4cbcff1c04
mkoner <amzid@hotmail.fr>
2022-04-23 04:52:37 -0700

    conatenate2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,24 @@
 #include "main.h"
 /**
- * *_strcat - concatenates two strings
+ * *_strncat - concatenates n bytes of two strings
  * @dest: pointer destination
  * @src: pointer source
- * @n: number of chars of src to add
+ * @n: number of bytes
  * Return: void
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	        int i = 0, j = 0;
-		
-		for(i = 0; dest[i] != '\0'; i++)
-		for(j = 0; j < n && src[j] != '\0'; j++)
-			dest[i + j] = src[j];
-		dest[i + j] = '\0';
-		return (dest);
+
+int dest_len, i;
+for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[dest_len + i] = src[i];
+
+/*should end with a end of string char*/
+dest[dest_len + i] = '\0';
+
+return (dest);
 }
