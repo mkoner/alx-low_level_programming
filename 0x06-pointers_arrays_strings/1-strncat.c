3231ffd16f4a6722d8767af9d118d6fbdd9e754f
mkoner <amzid@hotmail.fr>
2022-04-23 04:50:46 -0700

    conatenate2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,8 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	        int i = 0, j = 0;
 		
-		while(dest[i] != '\0')
-			i++;
+		for(i = 0; dest[i] != '\0'; i++)
 		for(j = 0; j < n && src[j] != '\0'; j++)
 			dest[i + j] = src[j];
 		dest[i + j] = '\0';
