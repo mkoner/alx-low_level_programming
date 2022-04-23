44bc8c052f1bec63f64f58df9651f5957789df19
mkoner <amzid@hotmail.fr>
2022-04-23 04:29:29 -0700

    conatenate2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+include "main.h"
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n: number of chars of src to add
+ * Return: void
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	        int i = 0, j = 0;
+		
+		while(dest[i] != '\0')
+			i++;
+		while(j != n)
+		{
+			dest[i] = src[j];
+			i++;
+			j++;
+		}
+		dest[i] = '\0';
+		return (dest);
+}
