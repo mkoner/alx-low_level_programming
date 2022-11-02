6930aed8b5a4e3732f9ebc8758fce1e4e9c3efff
mkoner <amzid@hotmail.fr>
2022-11-02 15:03:19 +0000

    Task0 ..

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,10 +13,11 @@
  int linear_search(int *array, size_t size, int value) {
     int i;
     for (i = 0; i < size; i++){
+
         printf("Value checked array[%d] = [%d]", i, array[i]);
-        if (array[i] == value){
+        if (array[i] == value)
             return i;
-        }
-        return -1;
-    }
+ 
+    }    
+    return -1;
  }
