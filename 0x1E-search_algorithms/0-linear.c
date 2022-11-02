5867dc29f3ce97eaf16effc8587ed38a76e05f11
mkoner <amzid@hotmail.fr>
2022-11-02 15:10:23 +0000

    Task0 ..

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,7 +10,8 @@
  * Return: first index where value is located.
  */
 
- int linear_search(int *array, size_t size, int value) {
+ int linear_search(int *array, size_t size, int value)
+ {
 	int i;
 
 	if (array == NULL)
