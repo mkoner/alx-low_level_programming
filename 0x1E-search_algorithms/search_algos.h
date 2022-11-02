d87137fcc911df91bd9cf06025de2025e5d2263a
mkoner <amzid@hotmail.fr>
2022-11-02 15:05:52 +0000

    Task0 ..

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,8 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 
